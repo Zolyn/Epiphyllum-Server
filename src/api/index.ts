d18a1dd591b710b3298ef1881ef7ad4fe8a861df
Zorin <zorin1477@gmail.com>
2021-09-25 22:56:31 +0800

    test: vercel

--- src/api/index.ts
+++ src/api/index.ts
@@ -8,7 +8,7 @@ interface Response {
     data?: EpiphyllumEntryReturn;
 }
 
-export const app = express();
+const app = express();
 const cache = apicache.options({
     headers: {
         'cache-control': 'no-cache',
@@ -47,3 +47,5 @@ app.get('/', async (req, res) => {
 app.listen(3000, () => {
     Logger.info('Listening on http://localhost:3000');
 });
+
+module.exports = app;
