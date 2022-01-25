762d5412dc6b3060dffadfb211b2533d1d4dec8a
Zorin <zorin1477@gmail.com>
2022-01-25 18:16:39 +0800

    test: vercel

--- dist/epiphyllum.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Config = {
-    storage: 'upyun',
-    host: 'https://repo-zorin.beaa.cn',
-};
-exports.default = Config;
